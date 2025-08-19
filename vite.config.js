252b124ccd6bcdfe5dcb7fd827eb479f68c9e30b
herothegolden <herothegolden@gmail.com>
2025-08-19 14:32:25 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: true },      // so LAN devices/Telegram Web can hit your dev server
-  build: { outDir: 'dist' }    // Express serves this folder in production
-})
+  server: { host: true },
+  build: { outDir: "dist" }
+});
