2d8a377b319d30733524fe254c5fc9c41d348a50
herothegolden <herothegolden@gmail.com>
2025-08-19 17:51:31 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: true },
-  build: { outDir: 'dist' }
+  server: { 
+    host: true,
+    port: 3000
+  },
+  build: { 
+    outDir: 'dist',
+    sourcemap: false
+  }
 })
