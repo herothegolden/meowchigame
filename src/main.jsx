d5cea9a3c98e5a17af8911f3b716ca990f017059
herothegolden <herothegolden@gmail.com>
2025-08-20 00:09:11 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'   // <-- ensure this line exists
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
