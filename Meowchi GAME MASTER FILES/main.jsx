038d366cb7907ce8494204872fb73d64d747f71b
herothegolden <herothegolden@gmail.com>
2025-08-25 17:14:25 +0500

    Delete Meowchi GAME MASTER FILES directory

--- Meowchi GAME MASTER FILES/main.jsx
+++
@@ -1,6 +0,0 @@
-// src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'   // <-- ensure this line exists
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
