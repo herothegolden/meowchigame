21c7442e84f370fbabe4f6149f04f3033ee59403
herothegolden <herothegolden@gmail.com>
2025-08-28 02:28:11 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 // src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import store from './app/store.js';
 import App from './App.jsx'
 import './index.css'   // <-- ensure this line exists
 
@@ -20,7 +18,7 @@ if ('serviceWorker' in navigator) {
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 )
