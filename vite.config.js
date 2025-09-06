b4fa94a974bc4ab6df6f81348531e94fc8ff9922
herothegolden <herothegolden@gmail.com>
2025-09-07 02:12:24 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [],
+      output: {
+        manualChunks: undefined
+      }
+    },
+    target: 'es2015',
+    minify: 'esbuild'
+  },
+  server: {
+    port: 3000,
+    host: true
+  }
 })
