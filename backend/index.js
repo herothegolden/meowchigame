8ded267495f3372fdabe600b7c15f2e8fbbc4b69
herothegolden <herothegolden@gmail.com>
2025-09-28 22:13:16 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -328,11 +328,6 @@ const setupDatabase = async () => {
   }
 };
 
-// ---- EXPRESS APP ----
-const app = express();
-app.use(cors());
-app.use(express.json());
-
 // Serve uploaded files statically
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
