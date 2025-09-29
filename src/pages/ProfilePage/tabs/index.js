680135708d85a867e6b873848219038126c125a5
herothegolden <herothegolden@gmail.com>
2025-09-29 23:30:53 +0500

    Create index.js

---
+++ src/pages/ProfilePage/tabs/index.js
@@ -0,0 +1,4 @@
+export { default as OverviewTab } from './OverviewTab';
+export { default as BadgesTab } from './BadgesTab';
+export { default as LeaderboardTab } from './LeaderboardTab';
+export { default as TasksTab } from './TasksTab';
