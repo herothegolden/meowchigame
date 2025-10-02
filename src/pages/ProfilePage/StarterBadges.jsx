730aed4b2e903483e8f44f1a9bbdc4ee3b5e71c1
herothegolden <herothegolden@gmail.com>
2025-10-02 18:25:01 +0500

    Update StarterBadges.jsx

--- src/pages/ProfilePage/StarterBadges.jsx
+++ src/pages/ProfilePage/StarterBadges.jsx
@@ -28,7 +28,7 @@ const StarterBadges = () => {
               onClick={() => setActiveBadge(null)}
             />
             <motion.div
-              className="fixed top-[22%] left-1/2 -translate-x-1/2 z-50 w-[90vw] max-w-[400px]"
+              className="fixed top-[400px] left-1/2 -translate-x-1/2 z-50 w-[90vw] max-w-[400px]"
               initial={{ opacity: 0, scale: 0.9 }}
               animate={{ opacity: 1, scale: 1 }}
               exit={{ opacity: 0, scale: 0.9 }}
